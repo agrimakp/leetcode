1f4692442b45b03aea2ade469f7031be83635113
agrimakp <qvistn111@gmail.com>
2023-04-17 19:29:16 +1000

    35 binary search

---
+++ BinarySearch/35.Search.go
@@ -0,0 +1,23 @@
+package main
+
+func searchInsert(nums []int, target int) int {
+	low := 0
+	high := len(nums) - 1
+
+	for low <= high {
+		mid := low + (high-low)/2
+		guess := nums[mid]
+
+		if guess == target {
+			return mid
+		}
+
+		if guess > target {
+			high = mid - 1
+
+		} else {
+			low = mid + 1
+		}
+	}
+	return low
+}
